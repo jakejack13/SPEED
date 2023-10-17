2bf431f1ff1a1cc8a2c8a931a626ed56dfc5e005
Jacob Kerr <jck268@cornell.edu>
2023-10-17 14:40:54 -0400

    Slight adjust to annotations

--- workers/src/main/java/edu/cornell/repository/RepositoryImpl.java
+++ workers/src/main/java/edu/cornell/repository/RepositoryImpl.java
@@ -2,18 +2,14 @@ package edu.cornell.repository;
 
 import java.io.File;
 import java.util.List;
-import lombok.EqualsAndHashCode;
+import lombok.Data;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 
 /**
  * A class representing a Java project to build and test
  * @author Jacob Kerr
  */
-@EqualsAndHashCode
-@ToString
-@RequiredArgsConstructor
+@Data
 final class RepositoryImpl implements Repository {
     /** The root directory of the project once it's cloned */
     private final @NonNull File rootDir;
