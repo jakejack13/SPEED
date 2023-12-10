2903d1b840b324553bbce356330ade8be05ce3a4
Mitchell Gray <67762738+MitchellGray100@users.noreply.github.com>
2023-12-10 00:33:20 -0500

    Update Config.java

--- workers/src/main/java/edu/cornell/repository/Config.java
+++ workers/src/main/java/edu/cornell/repository/Config.java
@@ -9,7 +9,14 @@ import java.util.List;
  */
 public class Config {
 
+    /**
+    Parser for parsing config file.
+    */
     private ConfigParser parser;
+
+    /**
+    Creates config file.
+    */
     public Config(String configFilePath) throws ConfigSyntaxException, IOException {
         parser = new ConfigParser(Path.of(configFilePath));
     }
