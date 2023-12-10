0f3182f94076246e848f225d6b7fad026208091e
Mitchell Gray <67762738+MitchellGray100@users.noreply.github.com>
2023-12-10 00:31:53 -0500

    Update ConfigSyntaxException.java

--- workers/src/main/java/edu/cornell/repository/ConfigSyntaxException.java
+++ workers/src/main/java/edu/cornell/repository/ConfigSyntaxException.java
@@ -1,5 +1,8 @@
 package edu.cornell.repository;
 
+/**
+Thrown whenever syntax of the config file is incorrect.
+*/
 public class ConfigSyntaxException extends Exception {
     public ConfigSyntaxException(String message) {
         super(message);
