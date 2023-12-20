ff7be22a0acdf1d4d242e44596a395a4a6441525
Owen X. Ralbovsky <oxr2@cornell.edu>
2023-12-20 12:38:48 -0800

    Refactor: Separate out ExecutionListener

--- workers/src/main/java/edu/cornell/testenv/testrunner/JUnit5TestRunner.java
+++ workers/src/main/java/edu/cornell/testenv/testrunner/JUnit5TestRunner.java
@@ -32,68 +32,6 @@ import java.util.stream.Collectors;
 @Slf4j
 public class JUnit5TestRunner implements TestRunner {
 
-    @ToString
-    private static class OutputTestExecutionListener implements TestExecutionListener {
-
-        private final TestOutputStream outputStream;
-
-        OutputTestExecutionListener(TestOutputStream outputStream) {
-            this.outputStream = outputStream;
-        }
-
-        @Override
-        public void executionSkipped(TestIdentifier testIdentifier, String reason) {
-            System.out.println(testIdentifier.getDisplayName() + " skipped for " + reason);
-        }
-
-        @Override
-        public void executionFinished(TestIdentifier testIdentifier,
-                                      TestExecutionResult testExecutionResult) {
-            if(!testIdentifier.isTest()) { return; }
-
-            TestResult result = switch (testExecutionResult.getStatus()) {
-                case ABORTED -> TestResult.EXCEPTION;
-                case FAILED -> TestResult.FAILURE;
-                case SUCCESSFUL -> TestResult.SUCCESS;
-            };
-            outputStream.sendTestResult(testIdentifier.getDisplayName(), result);
-        }
-    }
-
-    private static class CustomTestExecutionListener implements TestExecutionListener {
-        @Override
-        public void testPlanExecutionStarted(TestPlan testPlan) {
-            LOGGER.info("Test plan execution started");
-        }
-
-        @Override
-        public void testPlanExecutionFinished(TestPlan testPlan) {
-            LOGGER.info("Test plan execution finished");
-        }
-
-        @Override
-        public void dynamicTestRegistered(TestIdentifier testIdentifier) {
-            LOGGER.info("Dynamic test registered: {}", testIdentifier);
-        }
-
-        @Override
-        public void executionSkipped(TestIdentifier testIdentifier, String reason) {
-            LOGGER.info("Execution skipped for test {}: {}", testIdentifier, reason);
-        }
-
-        @Override
-        public void executionStarted(TestIdentifier testIdentifier) {
-            LOGGER.info("Execution started for test: {}", testIdentifier);
-        }
-
-        @Override
-        public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
-            LOGGER.info("Execution finished for test {}: {}", testIdentifier, testExecutionResult);
-        }
-    }
-
-
-
     /**
      * Main runner that takes in the context and returns the result of JUnit Tests
      * @param context - The environment needed to run the tests
@@ -102,23 +40,20 @@ public class JUnit5TestRunner implements TestRunner {
     @Override
     public boolean runTest(TestEnvContext<String> context, TestOutputStream outputStream, File rootDir) {
         try {
+            //TODO: Assumption is class files live in /build/classes/java/ likely needing a resolve
             String baseRootPath = rootDir.toString() + "/build/classes/java/".replaceAll("/", File.separator);
-            String baseTestRootPath = baseRootPath + "test" + File.separator;
 
             try (LauncherSession session = LauncherFactory.openSession()) {
                 Launcher launcher = session.getLauncher();
                 SummaryGeneratingListener summaryListener = new SummaryGeneratingListener();
-                launcher.registerTestExecutionListeners(summaryListener, new CustomTestExecutionListener());
+                OutputTestExecutionListener outputTestExecutionListener = new OutputTestExecutionListener(outputStream);
+                launcher.registerTestExecutionListeners(summaryListener, outputTestExecutionListener);
 
                 JUnitContextClassLoader.loadClassesFromDirectory(baseRootPath);
 
                 List<DiscoverySelector> selectors = new ArrayList<>();
 
                 for (String classPath : context.getTestClasses()) {
-                    List<String> splitString = Arrays.asList(classPath.split("\\."));
-                    String classRootPath = String.join(File.separator, splitString) + ".class";
-                    String pathToTestClass = baseTestRootPath + classRootPath;
-
                     selectors.add(DiscoverySelectors.selectClass(classPath));
                 }
 
@@ -129,12 +64,6 @@ public class JUnit5TestRunner implements TestRunner {
                 launcher.execute(request);
 
                 TestExecutionSummary summary = summaryListener.getSummary();
-                LOGGER.info("Number of Tests Run: {}", summary.getTestsFoundCount());
-                LOGGER.info("Number of Tests Successful: {}", summary.getTestsSucceededCount());
-                LOGGER.info("Number of Tests Failed: {}", summary.getTestsFailedCount());
-                LOGGER.info("Number of Tests Skipped: {}", summary.getTestsSkippedCount());
-
-                // Return true if all tests passed
                 return summary.getTestsFailedCount() == 0 && summary.getTestsSkippedCount() == 0;
             }
 
@@ -143,53 +72,5 @@ public class JUnit5TestRunner implements TestRunner {
             return false;
         }
     }
-
-    private void executeTest(String classPath, Class<?> clazz, Launcher launcher) {
-//
-    }
-
-//    // Setup Launcher to find and builder test map for JUnit
-//    LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
-//            .selectors(selectors)
-//            .build();
-//
-//
-//            if(Main.DEBUG_MODE) {
-//        LOGGER.info("DEBUG: Printing Selectors");
-//        List<DiscoverySelector> selectorsList = request.getSelectorsByType(DiscoverySelector.class);
-//        for (DiscoverySelector selector : selectorsList) {
-//            LOGGER.info("DEBUG: Selector: {}", selector);
-//        }
-//    }
-//
-//    // Test Result Listeners
-//    SummaryGeneratingListener summaryListener = new SummaryGeneratingListener();
-//    OutputTestExecutionListener outputListener =
-//            new OutputTestExecutionListener(outputStream);
-//
-//            try (LauncherSession session = LauncherFactory.openSession()) {
-//        Launcher launcher = session.getLauncher();
-//        launcher.registerTestExecutionListeners(summaryListener, new CustomTestExecutionListener());
-//        TestPlan testPlan = launcher.discover(request);
-//
-//        testPlan.getRoots().forEach(root -> {
-//            LOGGER.info("Root: {}", root.getUniqueId());
-//            LOGGER.info("Children: {}", testPlan.getChildren(root.getUniqueId()));
-//            testPlan.getChildren(root.getUniqueId()).forEach(testIdentifier -> {
-//                LOGGER.info("Test Identifier: {}", testIdentifier);
-//            });
-//        });
-//
-//        launcher.execute(testPlan);
-//    }
-//
-//    TestExecutionSummary summary = summaryListener.getSummary();
-//
-//    // Print information about the run tests
-//            LOGGER.info("Number of Tests Run: {}", summary.getTestsFoundCount());
-//            LOGGER.info("Number of Tests Successful: {}", summary.getTestsSucceededCount());
-//            LOGGER.info("Number of Tests Failed: {}", summary.getTestsFailedCount());
-//            LOGGER.info("Number of Tests Skipped: {}", summary.getTestsSkippedCount());
-
 
 }
