3ee4282a9c5215dc928f94514b38111107d8b334
Owen X. Ralbovsky <oxr2@cornell.edu>
2023-12-20 20:48:37 -0800

    Serialize Path not valid exception

--- workers/src/main/java/edu/cornell/testenv/testcontext/PathIsNotValidException.java
+++ workers/src/main/java/edu/cornell/testenv/testcontext/PathIsNotValidException.java
@@ -1,9 +1,14 @@
 package edu.cornell.testenv.testcontext;
 
+import java.io.Serial;
+
 /**
  * Exception is thrown when the given path is not a directory or does not exist
  */
 public class PathIsNotValidException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1234L;
 
     /**
      * Creates a new PathIsNotValidException with the given message and backtrace
