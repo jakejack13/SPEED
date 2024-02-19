33e6bf8fc6bb01441fade00e1f137d03c9c35d38
Owen X. Ralbovsky <oxr2@cornell.edu>
2024-02-19 15:28:52 -0500

    Document TestResultsRecord in workers

--- workers/src/main/java/edu/cornell/testoutputstream/TestResultsRecord.java
+++ workers/src/main/java/edu/cornell/testoutputstream/TestResultsRecord.java
@@ -2,7 +2,16 @@ package edu.cornell.testoutputstream;
 
 import lombok.NonNull;
 
+/**
+ * Represents a test results record containing the result and elapsed time.
+ */
 public record TestResultsRecord(@NonNull String result, @NonNull Integer elapsedTime) {
+
+    /**
+     * Returns a string representation of the test results record.
+     *
+     * @return A string in the format "RESULT:{result};TIME_TAKEN:{elapsedTime}".
+     */
     @Override
     public String toString() {
         return "RESULT:" + result + ";TIME_TAKEN:" + elapsedTime;
