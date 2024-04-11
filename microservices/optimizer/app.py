e37e500162ce486a5bbffe8f7270659b1920e831
Mitchell Gray <meg346@cornell.edu>
2024-04-11 09:19:25 -0400

    Fixed printing

--- microservices/optimizer/app.py
+++ microservices/optimizer/app.py
@@ -62,7 +62,7 @@ def update_times() -> tuple[Response, int]:
             # Update database (Fixed in SPEED-41)
 
             app.logger.info(
-                "Updated %s in %s on branch %s with time %s", name, url, branch, time"
+                "Updated %s in %s on branch %s with time %s"
             )
         return (
             jsonify({"message": "Test class execution times updated successfully"}),
