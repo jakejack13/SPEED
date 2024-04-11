3e163d18a8b687e4cbfdee0854eb84bf7f92579c
Jacob Kerr <jck268@cornell.edu>
2024-04-10 21:23:55 -0400

    Switched to Docker local image caching

--- kubernetes/apply.sh
+++ kubernetes/apply.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-if [ "$#" -eq 0 ]; then
-    kubectl='microk8s kubectl'
-else
-    kubectl='kubectl'
-fi
+cd ..
+docker compose build
+docker push localhost:32000/speed-workers:registry
+docker push localhost:32000/speed-leaders:registry
+docker push localhost:32000/speed-first:registry
+docker push localhost:32000/speed-optimizer:registry
 
-$kubectl apply -f zoo-kafka-deployment.yaml,zoo-kafka-service.yaml
-$kubectl apply -f firstdb-deployment.yaml,firstdb-service.yaml
-$kubectl wait --for=condition=ready pod -l app=zoo-kafka --timeout 300s
-$kubectl wait --for=condition=ready pod -l app=firstdb --timeout 300s
-$kubectl apply -f first-deployment.yaml,first-service.yaml
-$kubectl apply -f optimizer-deployment.yaml,optimizer-service.yaml
-$kubectl wait --for=condition=ready pod -l app=first --timeout 300s
-$kubectl wait --for=condition=ready pod -l app=optimizer --timeout 300s
+cd kubernetes
+microk8s kubectl apply -f zoo-kafka-deployment.yaml,zoo-kafka-service.yaml
+microk8s kubectl apply -f firstdb-deployment.yaml,firstdb-service.yaml
+microk8s kubectl wait --for=condition=ready pod -l app=zoo-kafka --timeout 300s
+microk8s kubectl wait --for=condition=ready pod -l app=firstdb --timeout 300s
+microk8s kubectl apply -f first-deployment.yaml,first-service.yaml
+microk8s kubectl apply -f optimizer-deployment.yaml,optimizer-service.yaml
+microk8s kubectl wait --for=condition=ready pod -l app=first --timeout 300s
+microk8s kubectl wait --for=condition=ready pod -l app=optimizer --timeout 300s
