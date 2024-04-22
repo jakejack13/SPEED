0f7437a1335e0eac3cdcba0b9d6f8f05e0bd1cec
Mitchell Gray <67762738+MitchellGray100@users.noreply.github.com>
2024-04-22 16:29:27 -0400

    #40: [Optimizer] Create optDB (#118)
    Implements: #40
    
    ---------
    
    Co-authored-by: Jacob Kerr <jck268@cornell.edu>

--- microservices/optimizer/app.py
+++ microservices/optimizer/app.py
@@ -1,12 +1,18 @@
 """Optimizer: the optimization microservice. More information can be found
 in the documentation at `optimizer_api_doc.md`"""
 
-from flask import Flask, jsonify, Response, request
+from flask import Flask, jsonify, Response, request, g
 
-from utils import optimize
+from utils import DBManager, optimize
 from utils.partition import PartitionMethod
 
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request() -> None:
+    """Adds the database manager to request context"""
+    g.db_manager = DBManager()
 
 
 @app.route("/partition", methods=["POST"])
@@ -35,14 +41,47 @@ def partition_tests() -> tuple[Response, int]:
     return jsonify({"partitions": partitions}), 200
 
 
+# pylint: disable=too-many-return-statements
 @app.route("/update", methods=["POST"])
 def update_times() -> tuple[Response, int]:
     """The `update` endpoint. This endpoint takes in the url and branch
     associated with the repository along with the execution times of each test
     classes of a recent deployment. The optimizer service should add this
     execution time into the database for the given test classes."""
-    app.logger.info("update endpoint invoked")
-    return jsonify({"error": "unimplemented"}), 501
+    data = request.json
+    if data is None:
+        return jsonify({"error": "missing request body"}), 400
+
+    url = data.get("url")
+    branch = data.get("branch")
+    times = data.get("times")
+
+    if not url or not branch or times is None:
+        return jsonify({"error": "missing or invalid parameters"}), 400
+
+    try:
+        for test_class in times:
+            name = test_class.get("name")
+            time = test_class.get("time")
+            if name is None or time is None:
+                return (
+                    jsonify({"error": "missing test class name or execution time"}),
+                    400,
+                )
+
+            db_manager: DBManager | None = getattr(g, "db_manager", None)
+            if db_manager is not None:
+                db_manager.update_execution_time(url, branch, name, time)
+            else:
+                return jsonify({"Can not connect to optDB"}), 400
+        return (
+            jsonify({"message": "Test class execution times updated successfully"}),
+            200,
+        )
+    except KeyError:
+        return jsonify({"error": "Key error - missing data in request"}), 400
+    except ValueError:
+        return jsonify({"error": "Value error - invalid data type"}), 400
 
 
 if __name__ == "__main__":
