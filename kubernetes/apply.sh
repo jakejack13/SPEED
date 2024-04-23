fd67c3dbb9ee1a0fcbd7ebe3fc9576ff69a30674
Jacob Kerr <jck268@cornell.edu>
2024-04-23 11:20:52 -0400

    Added optdb services

--- kubernetes/apply.sh
+++ kubernetes/apply.sh
@@ -11,8 +11,10 @@ cd kubernetes
 microk8s kubectl apply -f rbac-authorization.yaml
 microk8s kubectl apply -f zoo-kafka-deployment.yaml,zoo-kafka-service.yaml
 microk8s kubectl apply -f firstdb-deployment.yaml,firstdb-service.yaml
+microk8s kubectl apply -f optdb-deployment.yaml,optdb-service.yaml
 microk8s kubectl wait --for=condition=ready pod -l app=zoo-kafka --timeout 300s
 microk8s kubectl wait --for=condition=ready pod -l app=firstdb --timeout 300s
+microk8s kubectl wait --for=condition=ready pod -l app=optdb --timeout 300s
 microk8s kubectl apply -f first-deployment.yaml,first-service.yaml
 microk8s kubectl apply -f optimizer-deployment.yaml,optimizer-service.yaml
 microk8s kubectl wait --for=condition=ready pod -l app=first --timeout 300s
--- kubernetes/delete.sh
+++ kubernetes/delete.sh
@@ -3,5 +3,6 @@
 microk8s kubectl delete -f rbac-authorization.yaml
 microk8s kubectl delete -f zoo-kafka-deployment.yaml,zoo-kafka-service.yaml
 microk8s kubectl delete -f firstdb-deployment.yaml,firstdb-service.yaml
+microk8s kubectl delete -f optdb-deployment.yaml,optdb-service.yaml
 microk8s kubectl delete -f first-deployment.yaml,first-service.yaml
 microk8s kubectl delete -f optimizer-deployment.yaml,optimizer-service.yaml
